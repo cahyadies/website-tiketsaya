f1206b23756e1942fa07024c56f1d62b7176991b
Eko Sugeng Cahyadi <ekosugengcahyadi@gmail.com>
2021-11-03 15:21:53 +0700

    create javascript files

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Type Data JavaScript
+// String, Number, Object
+
+// Type Data String
+var namaSaya = "Cahyadies";
+
+// Type Data Integer
+var umurSaya = 22;
+
+// Type Data Object
+var biodata = {
+    nama =  "Afifah",
+    umur = 25,
+    jenisKelamin = "Perempuan"
+};
